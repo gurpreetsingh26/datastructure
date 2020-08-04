92daf93c0ce2e157c8909010e124f0c1b0a64a6d
Gurpreet Singh <gurpreetsingh@Gurpreets-MacBook-Pro.local>
2020-08-03 22:21:33 -0700

    iterative insertion sort

--- sort/insertionSort.py
+++ sort/insertionSort.py
@@ -1,7 +1,9 @@
 
-def sort(arr, n):
+def sort_recursive(arr, n):
     """
     Insertion sort
+    Top down
+    Solution from n to n -2
     :param arr:
     :param n:
     :return:
@@ -9,7 +11,7 @@ def sort(arr, n):
     if n <= 1:
         return
 
-    sort(arr, n-1)
+    sort_recursive(arr, n-1)
 
     j = n - 1
     element = arr[j]
@@ -19,16 +21,44 @@ def sort(arr, n):
     arr[j] = element
     return arr
 
+def sort_iterative(arr):
+    """
+    Insertion sort
+    Top down
+    Solution from n to n -2
+    :param arr:
+    :param n:
+    :return:
+    """
+    if len(arr) <= 1:
+        return arr
+
+    for i in range(1, len(arr)):
+        j = i
+        element = arr[i]
+        while j >= 1 and arr[j-1] > arr[j]:
+            arr[j] = arr[j-1]
+            j = j - 1
+        arr[j] = element
+    return arr
+
 def test():
     """
     Test sorting
     :return:
     """
-    arr = [2,1,8,0,-12,12]
-    sorted_arr = sort(arr, len(arr))
+    arr = [0,1,8,2,-12,12]
 
+    sorted_arr = sort_recursive(arr, len(arr))
     if arr == sorted_arr:
-        print "Works as expected"
+        print "Recursive Works as expected"
+    else:
+        print sorted_arr
+        print "Check for logic"
+
+    sorted_arr = sort_iterative(arr)
+    if arr == sorted_arr:
+        print "Iterative Works as expected"
     else:
         print sorted_arr
         print "Check for logic"
